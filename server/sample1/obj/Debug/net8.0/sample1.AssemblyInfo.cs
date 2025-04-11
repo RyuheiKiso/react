c47a6139e4a3b24b792cb4f8095ec74d67df7221
ryuhei.kiso <ryuhei.kiso@kubota.com>
2025-04-11 09:26:29 +0900

    Add configuration files for color, endpoint, and database settings

--- server/sample1/obj/Debug/net8.0/sample1.AssemblyInfo.cs
+++ server/sample1/obj/Debug/net8.0/sample1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595a6ac66ded6830646cabc622b97f666f383b64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82028345d44f5419e171df21d4b6ce491c8ecb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
