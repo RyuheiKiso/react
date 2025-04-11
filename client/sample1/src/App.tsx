0412700af262ec6167c69d93bf1a64eecf9fe9ed
ryuhei.kiso <ryuhei.kiso@kubota.com>
2025-04-11 16:26:23 +0900

    feat: CustomTextFieldコンポーネントをtextbox.tsxに移動し、Appコンポーネントでの使用を更新

--- client/sample1/src/App.tsx
+++ client/sample1/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomButton from './shared/components/button';
-import CustomTextField from './shared/components/textfield';
+import CustomTextField from './shared/components/textbox';
 
 function App() {
   return (
     <div className="App">
       <CustomButton label={'いいいいい'} triggerKey='F1' onClick={() => console.log('ボタンがクリックされました')} />
-      <CustomTextField placeholder={'テキストボックス'} />
+      <CustomTextField placeholder={'あああああ'} />
     </div>
   );
 }
